70faf2e795f883554a35edac9d917be3a8810517
Shaun Mullis <shmulli@microsoft.com>
2020-03-17 14:50:56 -0400

    Add parameter for CSE Storage Account Container

--- ARMTemplates/VMScaleSets/BindSSLToIIS.ps1
+++ ARMTemplates/VMScaleSets/BindSSLToIIS.ps1
@@ -4,7 +4,7 @@ param (
     [string]$CertSubjectName
 )
 
+# Can be added if not using image with IIS Preinstalled
 #Add-WindowsFeature Web-Server -IncludeManagementTools
-# Above can be added if not using image with IIS Preinstalled
 New-WebBinding -Name $IISSiteName -Protocol https -Port 443
 Get-ChildItem Cert:\LocalMachine\My | Where-Object {$_.Subject.ToLower().IndexOf($CertSubjectName.ToLower()) -gt 0} | New-Item -Path IIS:\SslBindings\!443
